97ccac946e21680779c2be002e99d4c399572286
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-09-25 12:28:52 -0700

    Add zig build script

---
+++ build.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+    const shared = b.option(bool, "zlib-shared", "Whether to build zlib as a shared library") orelse false;
+
+    const srcs = &.{
+        "adler32.c",
+        "compress.c",
+        "crc32.c",
+        "deflate.c",
+        "gzclose.c",
+        "gzlib.c",
+        "gzread.c",
+        "gzwrite.c",
+        "inflate.c",
+        "infback.c",
+        "inftrees.c",
+        "inffast.c",
+        "trees.c",
+        "uncompr.c",
+        "zutil.c",
+    };
+
+    const options = .{
+        .name = "z",
+        .target = target,
+        .optimize = optimize,
+    };
+
+    const zlib = if (shared)
+        b.addSharedLibrary(options)
+    else
+        b.addStaticLibrary(options);
+
+    zlib.linkLibC();
+
+    zlib.defineCMacro("_LARGEFILE64_SOURCE", "1");
+
+    if (target.result.abi == .msvc) {
+        zlib.defineCMacro("_CRT_SECURE_NO_DEPRECATE", "1");
+        zlib.defineCMacro("_CRT_NONSTDC_NO_DEPRECATE", "1");
+    }
+
+    zlib.addCSourceFiles(.{
+        .files = srcs,
+        .flags = &.{},
+    });
+
+    b.installArtifact(zlib);
+}
